9960ad4d85c03fce75fe583d27bc6ffa2ace23be
sadhanala Sai Gangadhar <gangadhar.sadhanala24@sasi.ac.in>
2026-07-04 09:45:29 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,a[100],temp;
+    printf("enter the size of an array:");
+    scanf("%d",&n);
+    printf("enter the elements of an array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("the sorted array elements:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
